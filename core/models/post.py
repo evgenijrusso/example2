cff370dc1b12486265ca804cc9dbdd3dc8257189
Rasen Nikov <rasen800@gmail.com>
2024-10-28 17:32:36 +0600

    Создание связи относительно mixins.py

--- core/models/post.py
+++ core/models/post.py
@@ -1,14 +1,19 @@
-from typing import TYPE_CHECKING
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+# from typing import TYPE_CHECKING
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 from .database import Base
+from .mixins import UserRelationMixin
 
-if TYPE_CHECKING:
-    from .user import User
+# if TYPE_CHECKING:
+#     from .user import User
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_back_populated = "posts"
+
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    user: Mapped["User"] = relationship(back_populates="posts")
+
+
+#  user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)   # эти поля используются в mixins
+#  user: Mapped["User"] = relationship(back_populates="posts")
